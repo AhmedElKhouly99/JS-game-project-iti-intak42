be7c326eb853564db3d18b0896ce1dde5b20a486
salma <salma.moamed.abdelatty@gmail.com>
2022-01-14 23:29:26 +0200

    some change

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ const info = document.getElementById('how-to-play');
 const pause = document.getElementById('pause-div');
 const mode = document.getElementById('mode');
 const single = document.getElementById('single');
-const multi =  document.getElementById("multi");
+const multi = document.getElementById("multi");
 
 const restart = document.getElementById('restart-img');
 
@@ -31,7 +31,7 @@ const c = new Audio('c.wav');
 music.setAttribute('loop', 'true');
 let level = 1;
 let setting_menu = false;
-
+let lives = 3;
 var playerNames = [];
 
 function playMusic() {
@@ -122,7 +122,7 @@ document.getElementById('sound').addEventListener('click', () => {
         b.muted = false;
         c.muted = false;
         document.getElementById("sound").src = "images/Sound_BTN_active.png";
-        
+
     } else {
         b.muted = true;
         c.muted = true
@@ -143,7 +143,7 @@ document.getElementById('music').addEventListener('click', () => {
 
 
 /// Single mode
-loginS.addEventListener('submit', (e)=>{
+loginS.addEventListener('submit', (e) => {
     e.preventDefault();
     removeTemplate(usernameS);
     removeDiv(loginS);
@@ -159,7 +159,7 @@ loginS.addEventListener('submit', (e)=>{
 
 
 ///// multiplayer mode
-loginM.addEventListener('submit', (e)=>{
+loginM.addEventListener('submit', (e) => {
     e.preventDefault();
     removeTemplate(usernameM);
     removeDiv(loginM);
@@ -176,13 +176,13 @@ loginM.addEventListener('submit', (e)=>{
 
 
 ///// Restart
-restart.addEventListener('click', ()=>{
+restart.addEventListener('click', () => {
 
 
     startNewGame(['islam']);
     console.log("Restarting Game");
     startNewGame();
-    
+
 });
 
 
@@ -202,10 +202,10 @@ var birds = [];
 var bullets = [];
 
 var multiplayer = false;
-var players;
+var players = [];
 
 
-var keyFlags = {'w' : false, 's' : false, 'a' : false, 'd' : false, ' ' : false, 'f' : false, 'ArrowUp' : false, 'ArrowDown' : false, 'ArrowLeft' : false, 'ArrowRight' : false};
+var keyFlags = { 'w': false, 's': false, 'a': false, 'd': false, ' ': false, 'f': false, 'ArrowUp': false, 'ArrowDown': false, 'ArrowLeft': false, 'ArrowRight': false };
 
 const delayBetweenFire = 200;
 
@@ -242,8 +242,8 @@ class Bird {
 }
 
 
-const player1Conrollers = {up:'ArrowUp', down:'ArrowDown', right:'ArrowRight', left : 'ArrowLeft', fire : ' '};
-const player2Conrollers = {up:'w', down:'s', right:'d', left : 'a', fire : 'f'};
+const player1Conrollers = { up: 'ArrowUp', down: 'ArrowDown', right: 'ArrowRight', left: 'ArrowLeft', fire: ' ' };
+const player2Conrollers = { up: 'w', down: 's', right: 'd', left: 'a', fire: 'f' };
 
 class Player {
     constructor(playerStyle, username) {
@@ -255,18 +255,18 @@ class Player {
         this.speedY = 10;
 
         this.playerImage = new Image();
-        
 
-        if(playerStyle == 1){
+
+        if (playerStyle == 1) {
             this.playerImage.src = 'player1.png';
             this.controllers = player1Conrollers;
         }
-        else{
+        else {
             this.playerImage.src = 'player2.png';
             this.controllers = player2Conrollers;
         }
 
-        
+
 
         this.movingLeft = false;
         this.movingRight = false;
@@ -279,48 +279,48 @@ class Player {
         this.lives = 3;
 
         this.username = username;
-        
+
     }
 
-    getScore(){
+    getScore() {
         return this.score;
     }
 
-    incrementScore(){
+    incrementScore() {
         this.score++;
     }
 
-    updateControllersStates(){
+    updateControllersStates() {
         this.movingUp = keyFlags[this.controllers.up];
         this.movingDown = keyFlags[this.controllers.down];
         this.movingRight = keyFlags[this.controllers.right];
         this.movingLeft = keyFlags[this.controllers.left];
         this.firing = keyFlags[this.controllers.fire];
-        
+
     }
 
-    updateState(){
+    updateState() {
         this.updateControllersStates();
         //Move if moving and fire if firing
 
-        if(this.movingUp){
+        if (this.movingUp) {
             this.moveUp();
         }
-        if(this.movingDown){
+        if (this.movingDown) {
             this.moveDown();
         }
-        if(this.movingRight){
-            
+        if (this.movingRight) {
+
             this.moveRight();
         }
-        if(this.movingLeft){
+        if (this.movingLeft) {
             this.moveLeft();
         }
-        if(this.firing){
-            if(this.canFire){
+        if (this.firing) {
+            if (this.canFire) {
                 this.fire();
                 this.canFire = false;
-                setTimeout(() => {this.canFire = true}, delayBetweenFire);
+                setTimeout(() => { this.canFire = true }, delayBetweenFire);
             }
         }
     }
@@ -339,12 +339,12 @@ class Player {
         this.currentX = Math.max(this.currentX - this.speedX, 0);
 
     }
-    moveUp(){
+    moveUp() {
         this.currentY = Math.max(this.currentY - this.speedY, 0);
         console.log("Moving Up");
     }
-    
-    moveDown(){
+
+    moveDown() {
         this.currentY = Math.min(this.currentY + this.speedY, canvasHeight - this.height);
         console.log("Moving Down");
     }
@@ -380,7 +380,7 @@ class Bullet {
         this.fireImage = new Image();
         this.fireImage.src = 'fire.png';
 
-        
+
     }
     move() {
         this.currentX += this.speedX;
@@ -408,14 +408,14 @@ function test() {
     deleteCrossed();
     detectBirdCollision();
     filtering();
-    updateScore();
+
 
     players.forEach((player) => {
         player.updateState();
         player.draw();
     });
 
-    
+    updateScore(multiplayer, players);
 
     [...birds, ...bullets].forEach((bird) => { bird.move(); bird.draw(); })//instead of using two lines
 
@@ -469,16 +469,31 @@ setInterval(() => {
 
 function deleteCrossed() {
 
-    [...birds, ...bullets].forEach((object) => {
-        if (object.currentX < (0 - object.width)) {
-            object.crossed = true;//remove birds that crossed the window
-            crossedBirds++;
-        }
-        if (object.currentY < (0 - object.height)) {
-            object.crossedHiegt = true;//remove fire from array when exceed canvas height
 
-        }
-    })
+    players.forEach((player) => {
+
+        birds.forEach((bird) => {
+
+            if (isPointInRectangle(bird.currentX, bird.currentY, player.currentX, player.currentY, player.width, player.height) ||
+                isPointInRectangle(bird.currentX + bird.width, bird.currentY, player.currentX, player.currentY, player.width, player.height) ||
+
+                isPointInRectangle(bird.currentX, bird.currentY + bird.height, player.currentX, player.currentY, player.width, player.height) ||
+
+                isPointInRectangle(bird.currentX + bird.width, bird.currentY + bird.height, player.currentX, player.currentY, player.width, player.height)
+            ) {
+                console.log("-------------------------")
+                player.lives--;
+                console.log(player.lives);
+                bird.crossed = true;
+            }
+            if (bird.currentX < (0 - bird.width)) {
+                bird.crossed = true;//remove birds that crossed the window
+                crossedBirds++;
+                player.lives--;
+            }
+        });
+
+    });
 }
 
 
@@ -505,12 +520,46 @@ window.addEventListener('blur', () => {
 
 
 
-function updateScore() {
+function updateScore(multiplayer, Player) {
     artArea.fillStyle = "#c2d2d4";
-    artArea.font = "30px myfont";
-    artArea.fillText('score:' + sCore, 50, 50);
+    artArea.font = "28px myfont";
+
+    let player1 = Player[0];
+    let player2 = Player[1];
+    if (!multiplayer) {
+        let x = 50;
+        let y = 50;
+
+        artArea.fillText(player1.username + " 's score : " + player1.score, x, y);
+        drawLives(player1,canvasWidth-80,40);
+    }
+    else {
+
+        artArea.fillText(player1.username + " 's score : " + player1.score, 50, 50);
+        drawLives(player1,200,70);
+        artArea.fillText(player2.username + " 's score : " + player2.score, canvasWidth - 450, 50);
+        drawLives(player2,canvasWidth-300,70);
+    }
 }
 
+function drawLives(player, px, py) {
+    let lifes = new Image();
+    if (player.lives == 3) {
+        lifes.src = "lives3.png";
+        artArea.drawImage(lifes,px,py,100,50 );
+    }
+    else if (player.lives == 2) {
+        lifes.src = "life2.png";
+        artArea.drawImage(lifes,px,py,100,50);
+    }
+    else if (player.lives == 1) {
+        lifes.src = "life1.png";
+        artArea.drawImage(lifes,px,py,50,50);
+    }
+    else {
+        play = 0;
+    }
+}
 
 
 
@@ -534,7 +583,7 @@ window.addEventListener('keydown', (e) => {
 
 
 
-window.addEventListener('keyup', (e)=> {
+window.addEventListener('keyup', (e) => {
     keyFlags[e.key] = false;
 });
 
@@ -544,13 +593,13 @@ window.addEventListener('keyup', (e)=> {
 setInterval(test, 25);      //generate  new frame every 30ms
 
 
-function startNewGame(){
+function startNewGame() {
     artArea.clearRect(0, 0, canvasWidth, canvasWidth);
 
-    if(multiplayer == false){   //Single Player
+    if (multiplayer == false) {   //Single Player
         players = [new Player(1, playerNames[0])];
     }
-    else{
+    else {
         players = [new Player(1, playerNames[0]), new Player(2, playerNames[1])];
     }
     crossedBirds = 0;
